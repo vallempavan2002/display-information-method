cc3ab63551237754737673319d74f957c1d65a61
Pavan kalyan Vallem <116752882+vallempavan2002@users.noreply.github.com>
2023-03-08 14:41:06 +0530

    Create display information method.java

---
+++ display information method.java
@@ -0,0 +1,16 @@
+void displayInformation(){
+    Student name{
+        class Teststudent4{
+            public static void main( String[]args)
+            {
+                Student s1=new Student();
+                 Student s2=new Student();
+                 s1.insertRecord(111,"Karan"):
+                 s2.insertRecord(222,"s,jf"):
+                 s1.displayInformation();
+                 s2.displayInformation();
+                 
+            }
+        }
+    }
+}
